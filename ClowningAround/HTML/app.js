f95ade5e6827c61a208c77a4a9daf393830b92f2
Lace-Gang <rhall@student.neumont.edu>
2025-02-19 09:58:30 -0700

    Database Added

---
+++ ClowningAround/HTML/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDVA8eRlNOaZIsMEaJlypv-XVW8noEVVM",
+  authDomain: "clowning-around-40bd5.firebaseapp.com",
+  projectId: "clowning-around-40bd5",
+  storageBucket: "clowning-around-40bd5.firebasestorage.app",
+  messagingSenderId: "803045719396",
+  appId: "1:803045719396:web:5b5a4272c282a559810e71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log(app)
