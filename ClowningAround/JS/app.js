7a9a5f8e933ea2ebd8c54f5a07ff06b2e821185a
Lace-Gang <rhall@student.neumont.edu>
2025-03-04 10:10:37 -0700

    Pushing New Players and NPCs (with optional default values)

--- ClowningAround/JS/app.js
+++ ClowningAround/JS/app.js
@@ -29,11 +29,12 @@ const app = initializeApp(firebaseConfig);
 
 const database = getDatabase(app);
 const db = getDatabase();
-//console.log(database);
+console.log(database);
 
 //Setting Up References
-const fullRef = ref(database, 'server/saving-data/players');
+const fullRef = ref(database, 'server/saving-data');
 const playersRef = child( fullRef, 'players');
+const npcsRef = child(fullRef, 'npcs');
 
 //const refT2 = ref(database, 'server/saving-data/players')
 
@@ -80,8 +81,9 @@ const playersRef = child( fullRef, 'players');
 
 ////REMEMBER TO AWAIT ANY FUNCTION RETURNING DATA FROM DATABASE AT **ALL** LEVELS
 document.addEventListener('DOMContentLoaded',async function() {
-  pushNewPlayer("playerName05", "120")
-
+  //pushNewPlayer("playerName05", "120")
+  
+    //
     //setData(playersRef);
     //updateData(playersRef);
     ////console.log(getData(playersRef));
@@ -105,7 +107,7 @@ document.addEventListener('DOMContentLoaded',async function() {
 //          })
 //}
 
-function pushNewPlayer(playerName, playerHighScore, playerLevel = 1)
+function pushNewPlayer(playerName, playerHighScore = 0, playerLevel = 1)
 {
   //playersRef.push().set({
   push(playersRef, {
@@ -115,22 +117,58 @@ function pushNewPlayer(playerName, playerHighScore, playerLevel = 1)
   });
 }
 
+function createTestPlayersBranch()
+{
+  pushNewPlayer('Test01');
+  pushNewPlayer('Example02', 206, 4);
+  pushNewPlayer('Attempt03', 0, 2);
+  pushNewPlayer('Try04', 30);
+}
+
+function createTestNpcBranch()
+{
+  pushNewNpc('TestNPC', 'Test Dialogue One', 'Dialogue Option Two', 'Hello World');
+  pushNewNpc('SecondNPC', 'This is the Second NPC', 'This should not have any lines that TestNPC does', '>:P');
+  pushNewNpc('TestOneDefault', 'This NPC tests if the option 3 default value works', 'If it does, then option three will be an empty string');
+  pushNewNpc('TestBothDefaultValues', 'This NPC tests if the 2nd and third option defaults work');
+}
 
 
-function setPlayerData(arg)
+//update will not work. We'll have to use push
+function pushNewNpc(NPCname, option01, option02 = '', option03 = '')
+{
+  push(npcsRef, {
+    npcname: NPCname,
+    dialogue: {
+      dialogue01: option01,
+      dialogue02: option02,
+      dialogue03: option03
+    }  
+  });
+}
+
+function updatePlayerScore(playerName, newScore)
 {
 
-    console.log(arg);
-
-    set(arg, {
-            player1: {
-              name: "Test01"
-            },
-            player2: {
-              name: "Example02"
-            }
-          })
 }
+
+
+
+
+//function setPlayerData(arg)
+//{
+//
+//    console.log(arg);
+//
+//    set(arg, {
+//            player1: {
+//              name: "Test01"
+//            },
+//            player2: {
+//              name: "Example02"
+//            }
+//          })
+//}
 
 
 
